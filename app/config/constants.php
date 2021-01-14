924a42878274b7110e8207af6f3e66de31aa7365
Madis <madismaisalumm@gmail.com>
2021-01-14 13:52:46 +0200

    Lisatud kasutusele rakendusega seotud konstandid

---
+++ app/config/constants.php
@@ -0,0 +1,8 @@
+<?php
+// Project name
+define('PROJECT_NAME', 'pvk_vs20');
+// app root dir
+define('APROOT', dirname(dirname(__FILE__)));
+// URL root
+define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
+
