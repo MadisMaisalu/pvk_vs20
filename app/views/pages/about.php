323edb8e746c5cbce312ed691dfe6da09d3eec4c
Madis <madismaisalumm@gmail.com>
2021-01-17 22:26:59 +0200

    Lisatud kasutusele vaade päis ja jalus

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,1 +1,7 @@
+<?php
+require_once APROOT.'/views/inc/header.php';
+?>
 <p><?php echo $data['content']; ?></p>
+<?php
+require_once APROOT.'/views/inc/footer.php';
+?>
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,2 +1,8 @@
+<?php
+require_once APROOT.'/views/inc/header.php';
+?>
 <h1><?php echo $data['title'];?></h1>
 <p><?php echo $data['content'];?></p>
+<?php
+require_once APROOT.'/views/inc/footer.php';
+?>
