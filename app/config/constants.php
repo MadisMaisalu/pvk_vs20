07d8e761496b4e15eeccfb8cb3b632c4e1c95082
Madis <madismaisalumm@gmail.com>
2021-01-17 23:04:13 +0200

    Loodud ja testitud ühendus andmebaasiga

--- app/config/constants.php
+++ app/config/constants.php
@@ -6,3 +6,8 @@ define('APROOT', dirname(dirname(__FILE__)));
 // URL root
 define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
 
+//db params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'pvk_vs20');
